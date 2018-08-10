a8a42f39e7f13e5398f945811666787a5f14a132
Itang Sanjana <arwata@gmail.com>
2018-08-10 15:05:01 +0700

    init

---
+++ docs/scripts/main.js
@@ -0,0 +1,41 @@
+/**
+ *   Copyright 2018 Itang Sanjana
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+(function() {
+  'use strict';
+
+  // Check that service workers are registered
+  if ('serviceWorker' in navigator) {
+    // Use the window load event to keep the page load performant
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('sw.js');
+    });
+  }
+
+  // Your custom JavaScript goes here
+  window.addEventListener('load', () => {
+    console.log('[navigator] onLine', navigator.onLine);
+
+    const navigatorOnLine = document.querySelector('.navigator-onLine');
+
+    if (navigator.onLine === true) {
+      navigatorOnLine.textContent = "Online";
+    }
+    else {
+      navigatorOnLine.textContent = "Offline";
+    }
+  });
+})();
